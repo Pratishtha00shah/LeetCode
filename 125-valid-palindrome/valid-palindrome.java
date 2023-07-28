0a73b73e80d16afc553c6c2bcab08e8f1c694979
Pratishtha Shah <99353855+Pratishtha00shah@users.noreply.github.com>
2023-07-28 23:35:42 +0530

    Time: 2 ms (99.78%) | Memory: 41.8 MB (94.41%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,29 +1,52 @@
+// class Solution {
+//     public boolean isPalindrome(String s) {
+
+// 			if(s.isEmpty()) {
+// 				return false;
+// 			}
+//       int start = 0;
+// 				int last = s.length() - 1;
+// 				while(start <= last) {
+
+// 					char curFirst = s.charAt(start);
+// 					char curLast = s.charAt(last);
+
+// 					if(!Character.isLetterOrDigit(curFirst)) {
+// 						start++;
+// 					} else if(!Character.isLetterOrDigit(curLast)) {
+//         		last--;
+// 					} else {
+// 						if (Character.toLowerCase(curFirst) !=  Character.toLowerCase(curLast)) {
+// 						return false;
+// 					} 
+					
+// 					start++;
+// 					last--;
+// 				}
+// 		}
+// 		 return true;
+// 	}
+// }
+
 class Solution {
     public boolean isPalindrome(String s) {
-
-			if(s.isEmpty()) {
-				return false;
-			}
-      int start = 0;
-				int last = s.length() - 1;
-				while(start <= last) {
-
-					char curFirst = s.charAt(start);
-					char curLast = s.charAt(last);
-					
-					if(!Character.isLetterOrDigit(curFirst)) {
-						start++;
-					} else if(!Character.isLetterOrDigit(curLast)) {
-        		last--;
-					} else {
-						if (Character.toLowerCase(curFirst) !=  Character.toLowerCase(curLast)) {
-						return false;
-					} 
-					
-					start++;
-					last--;
-				}
-		}
-		 return true;
-	}
+        
+        int left = 0;
+        int right = s.length() - 1;
+        
+        while(left<right){
+            while(left<right && !Character.isLetterOrDigit(s.charAt(left))){
+                left++;
+            }
+            while(left<right && !Character.isLetterOrDigit(s.charAt(right))){
+                right--;
+            }
+            if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
 }
