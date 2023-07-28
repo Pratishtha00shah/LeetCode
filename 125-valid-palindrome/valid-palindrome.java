3782757caa3ec883c89c8a0d4b1ebafc1fc279fc
Pratishtha Shah <99353855+Pratishtha00shah@users.noreply.github.com>
2023-07-28 16:18:49 +0530

    Time: 2 ms (99.78%) | Memory: 41.9 MB (82.88%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+
+			if(s.isEmpty()) {
+				return false;
+			}
+      int start = 0;
+				int last = s.length() - 1;
+				while(start <= last) {
+
+					char curFirst = s.charAt(start);
+					char curLast = s.charAt(last);
+					if(!Character.isLetterOrDigit(curFirst)) {
+						start++;
+					} else if(!Character.isLetterOrDigit(curLast)) {
+        		last--;
+					} else {
+						if (Character.toLowerCase(curFirst) !=  Character.toLowerCase(curLast)) {
+						return false;
+					} 
+					
+					start++;
+					last--;
+				}
+		}
+		 return true;
+	}
+}
