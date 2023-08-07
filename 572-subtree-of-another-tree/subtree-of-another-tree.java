6c196333b35cdc9b0fba073ef3e83d6e1ae23ef8
Pratishtha Shah <99353855+Pratishtha00shah@users.noreply.github.com>
2023-08-07 22:53:37 +0530

    Time: 2 ms (98.60%) | Memory: 43.1 MB (73.55%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.java
+++ 572-subtree-of-another-tree/subtree-of-another-tree.java
@@ -15,29 +15,58 @@
  */
 
 
+// class Solution {
+//     public boolean isSubtree(TreeNode s, TreeNode t) {
+//         if (s == null) {
+//             return false;
+//         }
+//         // Check if the current tree is equal to the target tree
+//         if (isSameTree(s, t)) {
+//             return true;
+//         }
+//         // Recursively check if the target tree is a subtree of the left or right subtree of the current tree
+//         return isSubtree(s.left, t) || isSubtree(s.right, t);
+//     }
+    
+//     private boolean isSameTree(TreeNode p, TreeNode q) {
+//         // If both nodes are null, the trees are considered equal
+//         if (p == null && q == null) {
+//             return true;
+//         }
+//         // If one node is null and the other is not, the trees are not equal
+//         if (p == null || q == null) {
+//             return false;
+//         }
+//         // Check if the values of the current nodes are equal, and recursively check if the left and right subtrees are equal
+//         return (p.val == q.val) && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+//     }
+// }
+
 class Solution {
-    public boolean isSubtree(TreeNode s, TreeNode t) {
-        if (s == null) {
-            return false;
-        }
-        // Check if the current tree is equal to the target tree
-        if (isSameTree(s, t)) {
+    
+    public boolean isSame(TreeNode s, TreeNode t){
+        if(s == null && t == null){
             return true;
         }
-        // Recursively check if the target tree is a subtree of the left or right subtree of the current tree
-        return isSubtree(s.left, t) || isSubtree(s.right, t);
+        if(s == null || t == null){
+            return false;
+        }
+        if(s.val != t.val){
+            return false;
+        }
+        return isSame(s.left, t.left) && isSame(s.right, t.right);
+        
     }
     
-    private boolean isSameTree(TreeNode p, TreeNode q) {
-        // If both nodes are null, the trees are considered equal
-        if (p == null && q == null) {
-            return true;
-        }
-        // If one node is null and the other is not, the trees are not equal
-        if (p == null || q == null) {
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        
+        if(root == null){
             return false;
         }
-        // Check if the values of the current nodes are equal, and recursively check if the left and right subtrees are equal
-        return (p.val == q.val) && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        if(isSame(root, subRoot)){
+            return true;
+        }
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+        
     }
 }
