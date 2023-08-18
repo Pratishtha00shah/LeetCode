df81b0d6e8e53f8e29abf8dea4b90fa8a0497232
Pratishtha Shah <99353855+Pratishtha00shah@users.noreply.github.com>
2023-08-19 00:00:28 +0530

    Time: 68 ms (12.92%) | Memory: 46.1 MB (5.62%) - LeetSync

--- 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.java
+++ 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.java
@@ -7,26 +7,6 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-// public class Codec {
-
-//     // Encodes a tree to a single string.
-//     public String serialize(TreeNode root) {
-        
-//     }
-
-//     // Decodes your encoded data to tree.
-//     public TreeNode deserialize(String data) {
-        
-//     }
-// }
-
-// Your Codec object will be instantiated and called as such:
-// Codec ser = new Codec();
-// Codec deser = new Codec();
-// TreeNode ans = deser.deserialize(ser.serialize(root));
-
-
-
 
 public class Codec {
     public String recserialize(TreeNode root, String str){
@@ -43,7 +23,6 @@ public class Codec {
         return str;
     }
     public String serialize(TreeNode root) {
-        
         return recserialize(root, "");
         
         
@@ -68,3 +47,44 @@ public class Codec {
         return recdeserialize(strList);
     }
 }
+
+// public class Codec {
+//     public String recserialize(TreeNode root, String str){
+    
+//         if(root == null){
+//             str += "null,";
+//         }
+//         else{
+//             str += str.valueOf(root.val) +",";
+//             str = recserialize(root.left, str);
+//             str = recserialize(root.right, str);
+//         }
+        
+//         return str;
+//     }
+//     public String serialize(TreeNode root) {
+        
+//         return recserialize(root, "");
+        
+        
+//     }
+//     // Decodes your encoded data to tree.
+//     public TreeNode recdeserialize(List<String> strs){
+//         if(strs.get(0).equals("null")){
+//             strs.remove(0);
+//             return null;
+//         }
+//         TreeNode root = new TreeNode(Integer.valueOf(strs.get(0)));
+//         strs.remove(0);
+//         root.left= recdeserialize(strs);
+//         root.right = recdeserialize(strs);
+        
+//         return root;
+//     }
+//     public TreeNode deserialize(String data) {
+        
+//         String[] strArray = data.split(",");
+//         List<String> strList = new LinkedList<String>(Arrays.asList(strArray));
+//         return recdeserialize(strList);
+//     }
+// }
