16c6d29dc06d95412e954e645d950fe2dec14726
Pratishtha Shah <99353855+Pratishtha00shah@users.noreply.github.com>
2023-08-30 23:06:05 +0530

    Time: 1 ms (97.07%) | Memory: 44.1 MB (8.25%) - LeetSync

--- 1651-shuffle-string/shuffle-string.java
+++ 1651-shuffle-string/shuffle-string.java
@@ -1,12 +1,29 @@
+//  class Solution {
+//     public String restoreString(String s, int[] indices) {
+//         int n = s.length();
+//         char arr[] = new char[n];
+//         for(int e=0; e<n; e++)
+//         {
+//             arr[indices[e]]=s.charAt(e);
+//         }
+
+//         return new String(arr);
+//     }
+// }
+
 class Solution {
     public String restoreString(String s, int[] indices) {
-        int n=s.length();
-        char arr[] = new char[n];
-        for(int e=0; e<n; e++)
-        {
-            arr[indices[e]]=s.charAt(e);
+        int n = s.length(); // Get the length of the original string
+        StringBuilder sb = new StringBuilder(s);
+         // Create a StringBuilder object and initialize with the original string
+
+        for(int i = 0; i < n; i++) {
+            sb.setCharAt(indices[i], s.charAt(i)); 
+            // Set the character at indices[i] to the character at position i in the original    string
         }
 
-        return new String(arr);
+        return sb.toString(); 
+        // Convert the StringBuilder object to a string and return the restored string
     }
 }
+
