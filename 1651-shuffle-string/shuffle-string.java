e015902ad2a908b81814e9f93e929c8334a06e39
Pratishtha Shah <99353855+Pratishtha00shah@users.noreply.github.com>
2023-09-02 21:02:38 +0530

    Time: 1 ms (96.70%) | Memory: 43.8 MB (38.69%) - LeetSync

--- 1651-shuffle-string/shuffle-string.java
+++ 1651-shuffle-string/shuffle-string.java
@@ -11,20 +11,6 @@
 //     }
 // }
 
-// class Solution {
-//     public String restoreString(String s, int[] indices) {
-//         int n = s.length();
-//         StringBuilder sb = new StringBuilder(s);
-
-//         for(int i = 0; i < n; i++) {
-//             sb.setCharAt(indices[i], s.charAt(i));
-//         }
-
-//         return sb.toString();
-//     }
-// }
-
-
 class Solution {
   public String restoreString(String s, int[] indices) {
     int n = s.length();
